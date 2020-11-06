f3d795076730c5a7a2f62c966923a466dc482083
Mike <michael-wilkins@sky.com>
2020-11-06 21:23:47 +0000

    Added GetAllDiets and GetDiet unit tests

--- DataLayer.Tests/SQLDietTest.cs
+++ DataLayer.Tests/SQLDietTest.cs
@@ -2,19 +2,16 @@ using DataLayer.Models;
 using DataLayer.Repositories;
 using DataLayer.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.IdentityModel.Tokens;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
 namespace DataLayer.Tests
 {
-        public class SQLDietTest
+    public class SQLDietTest
+    {
+        List<DietType> DietTypeInMemoryDb()
         {
-            List<DietType> DietTypeInMemoryDb()
-            {
             List<DietType> dietTypeList = new List<DietType>()
                 {
                     new DietType
@@ -29,38 +26,59 @@ namespace DataLayer.Tests
                     }
                 };
 
-                return dietTypeList;
-            }
-
-            [Fact]
-            public async void SQLDietRepository_Should_Add_DietType()
-            {
-                IDietRepository sut = GetInMemoryDietRepository();
-                List<DietType> dietType = DietTypeInMemoryDb();
-
-                DietType savedDietType = await sut.Add(dietType[0]);
-
-                Assert.Single(await sut.GetAllDiets());
-                Assert.Equal(1, savedDietType.Id);
-                Assert.Equal("Bland Diet", savedDietType.Name);
-
-            }
-
-
-            private IDietRepository GetInMemoryDietRepository()
-            {
-                DbContextOptions<ApplicationDbContext> options;
-                var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-                builder.UseInMemoryDatabase(databaseName: "DietTypeDatabase");
-           
-
-                options = builder.Options;
-                ApplicationDbContext dietTypeDataContext = new ApplicationDbContext(options);
-                dietTypeDataContext.Database.EnsureDeleted();
-                dietTypeDataContext.Database.EnsureCreated();
-
-                return new SQLDietRepository(dietTypeDataContext);
-            }
+            return dietTypeList;
         }
-    
+
+        [Fact]
+        public async void SQLDietRepository_Should_Add_DietType()
+        {
+            IDietRepository sut = GetInMemoryDietRepository();
+            List<DietType> dietType = DietTypeInMemoryDb();
+
+            DietType savedDietType = await sut.Add(dietType[0]);
+            DietType savedDietType2 = await sut.Add(dietType[1]);
+
+            var test = await sut.GetAllDiets();
+
+            Assert.Equal(2, test.Count());
+            Assert.Equal(1, savedDietType.Id);
+            Assert.Equal("Bland Diet", savedDietType.Name);
+            Assert.Equal(2, savedDietType2.Id);
+            Assert.Equal("Carb-counting Diet", savedDietType2.Name);
+
+        }
+
+        [Fact]
+        public async void SQLDietRepository_Should_Get_DietType_By_Id()
+        {
+            IDietRepository sut = GetInMemoryDietRepository();
+            List<DietType> dietType = DietTypeInMemoryDb();
+
+            DietType savedDietType = await sut.Add(dietType[0]);
+
+            var test = await sut.GetDiet(savedDietType.Id);
+            Assert.Single(await sut.GetAllDiets());
+            Assert.Equal(1, test.Id);
+            Assert.Equal("Bland Diet", test.Name);
+        }
+
+
+
+
+        private IDietRepository GetInMemoryDietRepository()
+        {
+            DbContextOptions<ApplicationDbContext> options;
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseInMemoryDatabase(databaseName: "DietTypeDatabase");
+
+
+            options = builder.Options;
+            ApplicationDbContext dietTypeDataContext = new ApplicationDbContext(options);
+            dietTypeDataContext.Database.EnsureDeleted();
+            dietTypeDataContext.Database.EnsureCreated();
+
+            return new SQLDietRepository(dietTypeDataContext);
+        }
+    }
+
 }
